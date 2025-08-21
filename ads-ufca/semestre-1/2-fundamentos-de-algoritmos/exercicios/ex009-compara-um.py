fa1655a3ae7dc543cceda3437a2895470722831e
phpablo <pablohenrique120@gmail.com>
2025-08-21 08:54:01 -0300

    :pencil: feat(exercise): add two algorithm exercises for fundamentals course
    - calculator for installment payments
    - program to check if number is even or odd

---
+++ ads-ufca/semestre-1/2-fundamentos-de-algoritmos/exercicios/ex009-compara-um.py
@@ -0,0 +1,14 @@
+print("================================")
+print("DIVISIVEL")
+print("================================")
+
+num = int(input("\nDigite um número:"))
+
+if num % 2 == 0:
+    print("\nEsse número é par!")
+else:
+    print("Esse número é impar!")
+
+print("================================")
+print("         FIM PROGRAMA           ")
+print("================================")
