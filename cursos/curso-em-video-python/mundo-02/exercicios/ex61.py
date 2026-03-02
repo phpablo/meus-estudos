9d64d4af9de448ae96b323e3b81fc1c1ae18bfae
phpablo <pablohenrique120@gmail.com>
2026-03-02 18:41:28 -0300

    Create ex61.py

---
+++ cursos/curso-em-video-python/mundo-02/exercicios/ex61.py
@@ -0,0 +1,12 @@
+print('Gerador de PA')
+print('-=' * 10)
+primeiro = int(input('Digite o primeiro termo da PA: '))
+razao = int(input('Digite a razão da PA: '))
+
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print(f'{termo} -> ', end='')
+    termo += razao
+    cont += 1
+print('FIM')
