cfa37af89df91846c39388c2bf5461e96dc4940a
phpablo <pablohenrique120@gmail.com>
2026-06-01 10:23:35 -0300

    ✏️

--- cursos/curso-em-video-python/mundo-03/exercicios/ex87.py
+++ cursos/curso-em-video-python/mundo-03/exercicios/ex87.py
@@ -2,6 +2,7 @@
  Crie um programa que declare uma matriz de dimensão 3×3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
 '''
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+spar = mai = scol = 0
 for i in range(0,3):
   for j in range(0,3):
     matriz[i][j] = int(input(f'Digite um valor para [{i}, {j}]: '))
@@ -10,4 +11,18 @@ print('Matriz: ')
 for i in range(0,3):
   for j in range(0,3):
     print(f'[{matriz[i][j]:^5}]', end='')
+    if matriz[i][j] % 2 == 0:
+      spar += matriz[i][j]
   print()
+print('-='*30)
+print(f'A soma dos valores pares é {spar}')
+for i in range(0,3):
+  scol += matriz[i][2]
+print(f'A soma dos valores da terceira coluna é {scol}')
+for j in range(0,3):
+  if j == 0:
+    mai = matriz[1][j]
+  elif matriz[1][j] > mai:
+    mai = matriz[1][j]
+print(f'O maior valor da segunda linha é {mai}')
+
