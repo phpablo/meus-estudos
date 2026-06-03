bfb45ad00d2eabcc82dd9c9245712663634f214d
phpablo <pablohenrique120@gmail.com>
2026-06-02 22:23:21 -0300

    📝

---
+++ cursos/curso-em-video-python/mundo-03/exercicios/ex88.py
@@ -0,0 +1,8 @@
+'''
+ Faça um programa que ajude um jogador da MEGA SENA a criar palpites.O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
+'''
+print('-'*30)
+print(f'JOGAR NA MEGA SENA')
+print('-'*30)
+res = int(input('Quantos jogos você quer que eu sorteie ? '))
+print(res)
